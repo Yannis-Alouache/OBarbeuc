b196353df82081cad1e45521f8a0cc4fe348c0a4
Yannis-Alouache <alouacehyannis1@gmail.com>
2022-02-25 14:16:16 +0100

    [+] added first commit

---
+++ obarbeuc/main/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class TodayLunch(models.Model):
+    title = models.CharField(max_length=50, blank=False, null=False)
+    price = models.IntegerField(blank=False, null=False)
+
+    def __str__(self):
+        return self.title
---
+++ obarbeuc/main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from .models import TodayLunch
+
+# Create your views here.
+def HomeView(request):
+    todayLunchs = TodayLunch.objects.all()
+    context = {
+        'todayLunchs' : todayLunchs
+    }
+    return render(request, "home.html", context)
