dfe200844cf32839201da28c78622bad03a45d93
RedCHK <redohk03@yahoo.com>
2022-03-02 11:03:20 +0100

    [+] added admin.py

--- obarbeuc/main/admin.py
+++ obarbeuc/main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import TodayLunch
+from .models import TodayLunch, Reservation
 
 # Register your models here.
 admin.site.register(TodayLunch)
+admin.site.register(Reservation)
